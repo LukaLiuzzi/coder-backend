0af3a90b65d609ddae103416a8f4664c9c5cfa84
Luka Liuzzi <lukaliuzzidev@gmail.com>
2022-09-05 19:59:45 -0300

    test con artillery, autocannon, 0x y node profiler. comandos, resultados e informe

--- src/routes/randomNumbers.js
+++ src/routes/randomNumbers.js
@@ -4,15 +4,17 @@ const { fork } = require('child_process');
 const path = require('path');
 
 router.get('/', (req, res) => {
-	const forked = fork(path.join(__dirname, '../childProcess/randomNumbers.js'));
+	// const forked = fork(path.join(__dirname, '../childProcess/randomNumbers.js'));
 
 	const cant = isNaN(req.query.cant) ? 100000000 : Number(req.query.cant);
 
-	forked.send(cant);
+	// forked.send(cant);
 
-	forked.on('message', (data) => {
-		res.json(data);
-	});
+	// forked.on('message', (data) => {
+	// 	res.json(data);
+	// });
+
+	res.json({ message: 'Ruta desactivada temporalmente' });
 });
 
 module.exports = router;
