f1c627556cb8da28a83d827f604172e8b7ac8539
Luka Liuzzi <lukaliuzzidev@gmail.com>
2022-09-10 17:49:01 -0300

    mongo url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
 	mongodb: {
 		connectionString:
-			'mongodb+srv://luka:luka1234@cluster0.8axswah.mongodb.net/ecommercecoder?retryWrites=true&w=majority',
+			'mongodb+srv://luka:luka1234@cluster0.9slvdpx.mongodb.net/ecommercecoder?retryWrites=true&w=majority',
 	},
 	firebase: {
 		type: process.env.TYPE,
